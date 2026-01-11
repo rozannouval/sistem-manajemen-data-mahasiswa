6ef26e6800f1d6692a31af77ed99f038261d1334
rifki <rifkisimatupang07@gmail.com>
2026-01-12 02:17:44 +0700

    menambahkan codingan footer.php dan header.php di layout.css

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -1,6 +1,20 @@
 <footer>
-    footer
+    <div class="container">
+        <div class="footer-content">
+            <div class="footer-info">
+                <p>&copy; 2026 <strong>SMDM</strong>. All Rights Reserved.</p>
+                <small>Sistem Manajemen Data Mahasiswa v1.0</small>
+            </div>
+            
+            <ul class="footer-links">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms of Service</a></li>
+            </ul>
+        </div>
+    </div>
 </footer>
 
+<script src="js/script.js"></script>
 </body>
 </html>
