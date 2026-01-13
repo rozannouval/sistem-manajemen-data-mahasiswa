0130cddd29559f93c86b7c16ac3a584002ad6ec1
Rozan Nouval <rozan@gmail.com>
2026-01-13 14:37:43 +0700

    Refactor CSS styles and update PHP logic for improved layout and data handling

--- php_logic/create.php
+++ php_logic/create.php
@@ -4,10 +4,10 @@ include "koneksi.php";
 if (isset($_POST['simpan'])) {
   mysqli_query($koneksi, "INSERT INTO mahasiswa VALUES (
         NULL,
-        '$_POST[nama]',
         '$_POST[nim]',
-        '$_POST[jurusan]',
+        '$_POST[nama]',
         '$_POST[email]',
+        '$_POST[jurusan]',
         '$_POST[alamat]',
         NOW()
     )");
--- views/includes/footer.php
+++ views/includes/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    footer
+    copyright by kelompok 1
 </footer>
 
 <script src="js/script.js"></script>
